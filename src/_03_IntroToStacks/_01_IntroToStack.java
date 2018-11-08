d571414788d81f3e7aee4160404be93c05ebf8dd
loganitis899 <league@iMac-2.attlocal.net>
2018-11-07 19:52:08 -0800

    finsihed
    start on hanging men

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -8,15 +8,27 @@ import javax.swing.JOptionPane;
 public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
+		Stack<Double> stack = new Stack();
 		//   Don't forget to import the Stack class
 		
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+	
+
+		for(int i=0;i<100;i++) {
+			stack.push(Math.random()*100);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		String int1 = JOptionPane.showInputDialog("enter a small # 0-100");
+		String int2 = JOptionPane.showInputDialog("enter a # Beeg 0-100");
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		int intone = Integer.parseInt(int1);
+		int inttwo = Integer.parseInt(int2);
+		for(int i=0; i<stack.size(); i++) {
+			double test = stack.pop();
+			if(test>intone&&test<inttwo) 
+			{System.out.println(test);
+		}
 		
 		//   EXAMPLE:
 		//   NUM 1: 65
@@ -32,4 +44,5 @@ public class _01_IntroToStack {
 		//   66.47984807
 		//   74.12121224
 	}
+}
 }
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,18 +1,79 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
-	/* 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	Stack<Character> stack = new Stack<Character>();
+
+	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
+	 * Every time a key is pressed, add that character to the JLabel. It should look
+	 * like a basic text editor.
 	 * 
-	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
-	 * Save that deleted character onto a Stack of Characters.
+	 * Make it so that every time the BACKSPACE key is pressed, the last character
+	 * is erased from the JLabel. Save that deleted character onto a Stack of
+	 * Characters.
 	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
-	 * off the Stack and added back to the JLabel.
+	 * Choose a key to be the Undo key. Make it so that when that key is pressed,
+	 * the top Character is popped off the Stack and added back to the JLabel.
 	 * 
-	 * */
-	
-	
+	 */
+	public static void main(String[] args) {
+		_02_TextUndoRedo start = new _02_TextUndoRedo();
+		start.start();
+	}
+
+	void start() {
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.setBackground(Color.green);
+		panel.add(label);
+		label.setBackground(Color.gray);
+		frame.addKeyListener(this);
+		frame.pack();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+System.out.println(stack.size());
+		char typed = e.getKeyChar();
+		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+
+			label.setText(label.getText().substring(0, label.getText().length() - 1));
+			stack.push(label.getText().charAt(label.getText().length()-1));
+		} else if(e.getKeyCode() == KeyEvent.VK_UP) {
+			Character pop = stack.pop();
+			String text = label.getText() + pop;
+			label.setText(text);
+			
+			System.out.println(pop);
+
+		} else {
+			label.setText(label.getText() + e.getKeyChar());
+		}
+		
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
