2c3d125eb16eeaac66d44c8f5da91fb20ca0e316
loganitis899 <league@WTS20.attlocal.net>
2018-11-10 11:28:36 -0800

    y
    hang

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -8,20 +8,43 @@ import java.util.Stack;
 import org.junit.Test;
 
 public class _03_TestMatchingBrackets {
+	Stack<Character> test = new Stack<Character>();
 
 	@Test
 	public void testMatchingBrackets() {
 		assertTrue(doBracketsMatch("{}"));
+		assertFalse(doBracketsMatch("}{"));
 		assertTrue(doBracketsMatch("{{}}"));
 		assertTrue(doBracketsMatch("{}{}{{}}"));
 		assertFalse(doBracketsMatch("{{}"));
-		assertFalse(doBracketsMatch("}{"));
+
 	}
 
-	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
+	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS
+	// A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
-		
-		return false;
-	}
+		char e = 'O';
+		test.empty();
+		for (int i = 0; i < b.length(); i++) {
 
+			if (b.charAt(i) == '{') {
+				test.push(e);
+			}
+			if (b.charAt(i) == '}') {
+				if (test.isEmpty()) {
+					return false;
+				} else {
+					test.pop();
+				}
+
+			}
+		}
+
+		if (test.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 }
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,15 @@
 package _04_HangMan;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class HangMan{
+	JFrame f = new JFrame();
+	JPanel p = new JPanel();
 	
+	public static void main(String[] args) {
+		
+	}
 	
 	
 }
