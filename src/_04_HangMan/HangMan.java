fe067be054b6b03cc0daac17545e2df300108497
loganitis899 <league@WTS20.attlocal.net>
2018-12-01 11:28:24 -0800

    yoi
    work on little stuff. Reset, lives, popping off, gaining dubs

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -15,13 +15,16 @@ public class HangMan implements KeyListener {
 	JLabel words = new JLabel();
 	JLabel lives = new JLabel();
 	String underscores = "";
-	int hearts;
+	int hearts = 5;
 	String poppedWord = "";
+	int L = 0;
 	Stack<String> word = new Stack<String>();
-
+ static Boolean done = false;
 	public static void main(String[] args) {
 		HangMan start = new HangMan();
 		start.start();
+		
+		
 
 	}
 
@@ -52,12 +55,41 @@ public class HangMan implements KeyListener {
 		char typed = e.getKeyChar();
 		for (int i = 0; i < poppedWord.length(); i++) {
 			if (typed == poppedWord.charAt(i)) {
-				words.getText().charAt(i) = poppedWord.charAt(i);
+				
+					if (i == 0) {
+					String partA=""+typed;
+					String partB = underscores.substring(1);
+					String returnWord = partA +partB;
+					words.setText(returnWord);
+					underscores=returnWord;
+					} else {
+						String partA = underscores.substring(0, i);
+						String partB = underscores.substring(i + 1);
+						String wordInsert = "" + typed;
+						String returnWord = partA + wordInsert + partB;
+						words.setText(returnWord);
+						underscores=returnWord;
+						if(done==true) {
+							reset();
+					}
+				
 			}
-
 		}
+if(underscores.contains("_")) {
+	
+} else {
+	done=true;
+}}
 	}
 
+	void reset() {
+		underscores="";
+		words.setText("");
+		setUpLength();
+		
+		
+		
+	}
 	@Override
 	public void keyPressed(KeyEvent e) {
 
@@ -70,11 +102,10 @@ public class HangMan implements KeyListener {
 
 	String setUpLength() {
 		poppedWord = word.pop();
-		String poppedWordCopy = poppedWord;
 		int lengthPoppedWord = poppedWord.length();
 
 		for (int i = 0; i < lengthPoppedWord; i++) {
-			underscores = underscores + "_ ";
+			underscores = underscores + "_";
 		}
 		System.out.println(underscores);
 		return underscores;
